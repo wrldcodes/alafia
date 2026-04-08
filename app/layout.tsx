03b348570db76e55bd1473cbed9f57f3f69bdf34
Jubril Elebute <jubrilelebute36@gmail.com>
2026-04-08 23:57:03 +0100

    Update OpenGraph image URL to remove file extension

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
     siteName: 'Alafia',
     images: [
       {
-        url: '/opengraph-image.png',
+        url: '/opengraph-image',
         width: 1200,
         height: 630,
         alt: 'Preview of Alafia platform',
@@ -27,3 +27,4 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   )
 }
+
