490c4bbe49c26ed4afebad1d4eea43528507d9e3
Jubril Elebute <jubrilelebute36@gmail.com>
2026-06-23 15:10:33 +0100

    fix build error 2.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en"
+    data-scroll-behavior="smooth"
+    >
       <head>
         <link
           rel="preload"
